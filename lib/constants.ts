bada4c2f5abd9d9be0148c2d4062e320d63163db
Kanan Farzaliyev <kanan.farzali@gmail.com>
2025-11-13 20:46:38 +0100

    implement home page

---
+++ lib/constants.ts
@@ -0,0 +1,98 @@
+export const events = [
+  {
+    slug: 'react-conf-2025',
+    image: '/images/event1.png',
+    title: 'React Conf 2025',
+    location: 'Las Vegas, NV',
+    date: 'May 15-16, 2025',
+    time: '9:00 AM - 6:00 PM'
+  },
+  {
+    slug: 'nextjs-conf',
+    image: '/images/event2.png',
+    title: 'Next.js Conf',
+    location: 'San Francisco, CA',
+    date: 'October 22, 2025',
+    time: '10:00 AM - 5:00 PM'
+  },
+  {
+    slug: 'jsconf-us',
+    image: '/images/event3.png',
+    title: 'JSConf US',
+    location: 'Austin, TX',
+    date: 'June 2-4, 2025',
+    time: '9:00 AM - 6:00 PM'
+  },
+  {
+    slug: 'ai-hackathon-sf',
+    image: '/images/event4.png',
+    title: 'AI Innovation Hackathon',
+    location: 'San Francisco, CA',
+    date: 'July 18-20, 2025',
+    time: '48 Hours'
+  },
+  {
+    slug: 'web3-summit',
+    image: '/images/event5.png',
+    title: 'Web3 Developer Summit',
+    location: 'Miami, FL',
+    date: 'August 8, 2025',
+    time: '9:00 AM - 6:00 PM'
+  },
+  {
+    slug: 'fullstack-europe',
+    image: '/images/event6.png',
+    title: 'Full Stack Europe',
+    location: 'Antwerp, Belgium',
+    date: 'September 18-20, 2025',
+    time: '9:00 AM - 6:00 PM'
+  },
+  {
+    slug: 'devops-world',
+    image: '/images/event1.png',
+    title: 'DevOps World',
+    location: 'Orlando, FL',
+    date: 'October 1-3, 2025',
+    time: '8:00 AM - 6:00 PM'
+  },
+  {
+    slug: 'wwdc',
+    image: '/images/event2.png',
+    title: 'Apple WWDC',
+    location: 'Cupertino, CA',
+    date: 'June 9-13, 2025',
+    time: '10:00 AM - 5:00 PM'
+  },
+  {
+    slug: 'google-io',
+    image: '/images/event3.png',
+    title: 'Google I/O',
+    location: 'Mountain View, CA',
+    date: 'May 14-15, 2025',
+    time: '9:00 AM - 6:00 PM'
+  },
+  {
+    slug: 'pycon-us',
+    image: '/images/event4.png',
+    title: 'PyCon US',
+    location: 'Pittsburgh, PA',
+    date: 'May 12-20, 2025',
+    time: '9:00 AM - 5:00 PM'
+  },
+  {
+    slug: 'dockercon',
+    image: '/images/event5.png',
+    title: 'DockerCon',
+    location: 'Los Angeles, CA',
+    date: 'September 4-5, 2025',
+    time: '8:30 AM - 6:00 PM'
+  },
+  {
+    slug: 'vue-conf-us',
+    image: '/images/event6.png',
+    title: 'VueConf US',
+    location: 'New Orleans, LA',
+    date: 'April 21-23, 2025',
+    time: '9:00 AM - 5:00 PM'
+  }
+]
