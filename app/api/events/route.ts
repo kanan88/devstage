70d85bbc286d813119a6dc2ea5efc903a05a0116
Kanan Farzaliyev <kanan.farzali@gmail.com>
2025-12-02 22:53:05 +0100

    implement cloudinary image upload

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,6 +1,8 @@
+import { v2 as cloudinary } from 'cloudinary'
+import { NextRequest, NextResponse } from 'next/server'
+
 import { Event } from '@/database'
 import { connectToDatabase } from '@/lib/mongodb'
-import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST(request: NextRequest) {
   try {
@@ -8,6 +10,34 @@ export async function POST(request: NextRequest) {
     const formData = await request.formData()
 
     const event = Object.fromEntries(formData.entries())
+
+    const file = formData.get('image') as File
+    if (!file) {
+      return NextResponse.json(
+        { message: 'Image is required' },
+        { status: 400 }
+      )
+    }
+
+    const arrayBuffer = await file.arrayBuffer()
+    const buffer = Buffer.from(arrayBuffer)
+
+    const uploadResult = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream(
+          { resource_type: 'image', folder: 'events' },
+          (error, result) => {
+            if (error) {
+              reject(error)
+            } else {
+              resolve(result)
+            }
+          }
+        )
+        .end(buffer)
+    })
+
+    event.image = (uploadResult as { secure_url: string }).secure_url
 
     const createdEvent = await Event.create(event)
 
